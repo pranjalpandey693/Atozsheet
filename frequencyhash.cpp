b9d394fa9e66d075474ded9622ca55775492c6b1
pranjalpandey693 <pranjalpandey693@gmail.com>
2023-02-19 15:13:10 +0530

    update

--- frequencyhash.cpp
+++ frequencyhash.cpp
@@ -19,10 +19,10 @@ class Solution{
     }
 };
 
-//without using extra space
+//without using extra space m2
 class Solution{
     public:
-    //Function to count the frequency of all elements from 1 to N in the array.
+
     void frequencyCount(vector<int>& arr,int n, int p)
     { 
         // code here
@@ -59,3 +59,24 @@ for(int j=0;j<n;j++)
         
     } 
 };
+
+//m3 
+void printfrequency(int arr[],int n)
+{
+    // Subtract 1 from every element so that the elements
+    // become in range from 0 to n-1
+    for (int j =0; j<n; j++)
+        arr[j] = arr[j]-1;
+ 
+    // Use every element arr[i] as index and add 'n' to
+    // element present at arr[i]%n to keep track of count of
+    // occurrences of arr[i]
+    for (int i=0; i<n; i++)
+        arr[arr[i]%n] = arr[arr[i]%n] + n;
+ 
+    // To print counts, simply print the number of times n
+    // was added at index corresponding to every element
+    for (int i =0; i<n; i++)
+        arr[i] = arr[i]/n ;
+}
+ 
