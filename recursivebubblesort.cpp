15a84511e7c1119373552eba400cb1c2070b668c
pranjalpandey693 <pranjalpandey693@gmail.com>
2023-03-16 22:14:13 +0530

    new

---
+++ recursivebubblesort.cpp
@@ -0,0 +1,18 @@
+  void bubbleSort(int arr[], int n)
+    {
+        // Your code here  
+    
+        if(n<=1)
+        return;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+        bubbleSort(arr,n-1);
+    }
