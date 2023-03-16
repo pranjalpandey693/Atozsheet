81b614c588bca4a63aa62aa6f6074f9f71c4248a
pranjalpandey693 <pranjalpandey693@gmail.com>
2023-03-16 17:49:51 +0530

    new

---
+++ selectionsort.cpp
@@ -0,0 +1,19 @@
+ void selectionSort(int arr[], int n)
+    {
+       //code here
+       
+    for(int i=0;i<=n-2;i++)
+    {     
+           int mini=i;
+        for(int j=i;j<=n-1;j++)
+        {   if(arr[mini]>arr[j])
+              {  
+              mini=j;
+                  
+              }
+              
+            
+        }
+        swap(arr[i],arr[mini]);
+        
+    }
