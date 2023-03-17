3cbad948276b36e21eb591f67de70e91e8c47027
pranjalpandey693 <pranjalpandey693@gmail.com>
2023-03-18 01:31:35 +0530

    update que

--- binarysearch/2maxrowof1s.cpp
+++ binarysearch/2maxrowof1s.cpp
@@ -1,5 +1,38 @@
 //binary search
+class Solution{
+public:
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    // code here
+	   int row=-1;
+	   int count =10000;
+	    for(int i=0;i<n;i++)
+	    {    int lo=0;
+	        int hi=m-1;
+	     
+	        while(hi-lo>=1)
+	        {    int mid=lo+(hi-lo)/2;
+	            if(arr[i][mid]==1)
+	        hi=mid;
+	        else if(arr[i][mid]==0)
+	        lo =mid+1;
+	            
+	        }
+	        if(arr[i][lo]==1 )
+	       {
+	           if(count>hi)
+	          { count =hi;
+	           row=i;
+	           
+	          }
+	        
+	       }
+	    }
+	    return row;
+	    
+	    
+	}
 
+};
 
 
 // brute force 
