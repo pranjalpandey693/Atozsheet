3868bf56b15c7ddcd39989a5cb15d2d2580566a9
pranjalpandey693 <pranjalpandey693@gmail.com>
2023-03-17 23:06:37 +0530

    new

---
+++ binarysearch/9peakelement.cpp
@@ -0,0 +1,28 @@
+// binary search 
+
+
+//brute force
+
+long long int no_of_subarrays(int n, vector<int> &arr) {
+    // Write your code here.
+    long long sum=0,x=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==0){
+            x++;
+        }
+        if(arr[i]==1){
+            sum+=((x+1)*x )/2;
+            x=0;
+            
+        }
+        if(arr[i]==0 && i==n-1){
+            sum+=((x+1)*x )/2;
+            x=0;
+            
+        }
+        
+       
+    }
+     return sum;
+}
