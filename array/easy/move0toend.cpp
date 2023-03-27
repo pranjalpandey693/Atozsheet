1256fe8bf7a5b263f548b9cf33c11e77d7f10f53
pranjalpandey693 <pranjalpandey693@gmail.com>
2023-03-27 22:53:52 +0530

    update que

--- array/easy/move0toend.cpp
+++ array/easy/move0toend.cpp
@@ -3,20 +3,41 @@ class Solution{
 public:
 	void pushZerosToEnd(int arr[], int n) {
 	    // code here
-	   vector<int> a;
-	   int count=0;
+ vector<int> a;
+	 
 	    for(int i=0;i<n;i++)
 	    {
 	        if(arr[i]!=0)
 	        a.push_back(arr[i]);
-	        else 
-	        count++;
+	      
       	}
-      	for(int i=0;i<count;i++)
+      	for(int i=a.size();i<n;i++)
       	a.push_back(0);
       	for(int i=0;i<n;i++)
       	arr[i]=a[i];
 	}
 };
 
-//method 2 using two pointer method
+//method 2 using two pointer to swap the elements
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    // code here
+	    int i=0,j=0;
+	    while(i<n)
+	    {
+	        if(arr[i]!=0)
+	        {
+	            int temp=arr[i];
+	            arr[i]=arr[j];
+	            arr[j]=temp;
+	            j++;
+	            i++;
+	        }
+	        else 
+	        i++;
+	    }
+	    for(int k=j;k<n;k++)
+	    arr[k]=0;
+	}
+};
